c5cc329026952bd09841ab4b24bd7d536344d34d
hakatashi <hakatasiloving@gmail.com>
2014-09-22 03:58:46 +0900

    Introduce clean task

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,6 +39,10 @@ module.exports = function (grunt) {
 				src: 'data/ivd.json',
 				dest: 'dist/ivd.json'
 			}
+		},
+		clean: {
+			dev: ['dev'],
+			dist: ['dist'],
 		}
 	});
 
@@ -46,12 +50,13 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-mocha-test');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-clean');
 
 	grunt.registerTask('default', ['dev']);
 
 	grunt.registerTask('data', []);
-	grunt.registerTask('dev', ['data', 'browserify:dev', 'copy:dev', 'uglify:dev']);
-	grunt.registerTask('dist', ['data', 'browserify:dist', 'copy:dist', 'uglify:dist']);
+	grunt.registerTask('dev', ['clean:dev', 'data', 'browserify:dev', 'copy:dev', 'uglify:dev']);
+	grunt.registerTask('dist', ['clean:dist', 'data', 'browserify:dist', 'copy:dist', 'uglify:dist']);
 	grunt.registerTask('test', ['data', 'mochaTest']);
 
 	grunt.registerTask('build', 'build ivs.json', function () {
